d195653a170fa8cd4224b5d6f09ef4585ea2400d
malashchuk <malashchuk.it@gmail.com>
2022-05-21 19:19:56 +0300

    feat: complete 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path'); 
+
+let folder = fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
+  if (err) throw err;
+  for (let i of files){
+    let pathOfFile = path.join(__dirname, 'secret-folder', `${i}`);
+    fs.stat(pathOfFile, (err, stats) => {
+      if (!stats.isDirectory()){
+        let file = i.split('.');
+
+        console.log(`${file[0]} - ${file[1]} - ${stats.size}b`)
+      }
+    });
+  }
+});
+
