e3f08bdb27563df6de3dd4e5688558ba788cc56a
malashchuk <malashchuk.it@gmail.com>
2022-05-22 13:24:23 +0300

    feat: complete 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+(async () => {
+  let allContent = [];
+
+  let files = await fs.promises.readdir(path.join(__dirname, 'styles'));
+
+  for (let file of files){
+    let fileName = file.split('.')
+    let type = fileName[1];
+    let stat = await fs.promises.stat(path.join(__dirname, 'styles', file));
+    if (type === 'css' && !stat.isDirectory()){
+      let content = await fs.promises.readFile(path.join(__dirname, 'styles', file), 'utf-8');
+      allContent.push(content + '\n');
+    //  console.log(content);
+    }
+  }
+
+  await fs.promises.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'), allContent);
+
+})();
